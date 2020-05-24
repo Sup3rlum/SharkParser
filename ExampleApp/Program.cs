ec904d3262c6445dbd310806a6de08930c123c13
Toma Kolev <S0207933@fecolleges.london>
2020-05-24 22:12:25 +0100

    added an example app

---
+++ ExampleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+using SharkParser;
+
+
+namespace ExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Initialize a new Context object, where we store contextual information such as variable names, values, function definitions, operation definitions and aliases.
+            Context c = new Context();
+
+            // To add a definition to the context simply input a string expression using generic mathematical expressions
+            c.AddDefinition("a=7.1");
+            c.AddDefinition("b=4.669");
+
+            // Next we create an expression object which encapsulates the internal tree structure generated from the input expression given by the specified grammar. If none selected - regular mathematical grammar will be used.
+            Expression x = new Expression("(2*a+b)^3");
+
+
+            // Evaluate our expression using the context our context and store the result. The expression is internally parsed into a tree as soon at is created thus making it immutable and completely functional. It can be applied to any set of inputs using any context, given that the context provides the necessary definitions.
+            double result = x.Evaluate(c);
+
+
+            // Output our result
+            Console.WriteLine($"The result is: {result}");
+
+
+        }
+    }
+}
